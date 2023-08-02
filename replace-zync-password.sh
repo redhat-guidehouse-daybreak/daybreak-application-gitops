c2c9a97d69c8128d420645f5bbec791ac160bfdd
Connie Peng <cpeng@guidehouse.com>
2023-08-02 18:24:29 -0400

    add test environment overlay

--- replace-zync-password.sh
+++ replace-zync-password.sh
@@ -2,7 +2,7 @@
 SECRET_VALUE=$(oc get secret zync-secret -n daybreak-gitops -o jsonpath="{.data.ZYNC_PASSWORD_$1}" | base64 --decode)
 
 # Replace the line in the file with the secret value
-FILE="components/argocd/applications/base/daybreak-applicationset"
+FILE="components/argocd/applications/overlays/$1/patch-applicationset"
 
 # Use sed to replace the line that starts with optional whitespace, followed by "zync_password:" and any characters
 # with the new line containing the secret value
